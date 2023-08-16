d62204769f1c425fa00434c57916da105c972910
KaiLangen96 <>
2023-08-16 13:48:59 +0000

    Admin, Signals & Forms Part 2

---
+++ checkout/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Order
+
+class OrderForm(froms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 'street_address1', 'street_address2', 'town_or_city', 'postcode', 'country', 'county')
