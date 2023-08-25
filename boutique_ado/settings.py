a872a94dde413116a4c28c04d56253eaedd4865f
KaiLangen96 <>
2023-08-25 08:36:31 +0000

    Caching, Media Files & Stripe 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,8 +182,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Cache controll:
     AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT'
-        'CacheControl': 'max-age=94608000'
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
     }
 
     # Bucket Config
